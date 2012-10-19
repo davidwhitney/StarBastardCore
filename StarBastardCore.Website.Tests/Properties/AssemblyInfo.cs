25c0261c309d2d2423e84928578a387353ba5afc
David Whitney <david@davidwhitney.co.uk>
2012-10-19 02:01:08 +0100

    removed old EDC bits.

--- StarBastardCore.Website.Tests/Properties/AssemblyInfo.cs
+++ StarBastardCore.Website.Tests/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Edc.Website.Tests")]
+[assembly: AssemblyTitle("StarBastardCore.Website.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Edc.Website.Tests")]
+[assembly: AssemblyProduct("StarBastardCore.Website.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- StarBastardCore.Website/Properties/AssemblyInfo.cs
+++ StarBastardCore.Website/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Edc.Website")]
+[assembly: AssemblyTitle("StarBastardCore.Website")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Edc.Website")]
+[assembly: AssemblyProduct("StarBastardCore.Website")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
