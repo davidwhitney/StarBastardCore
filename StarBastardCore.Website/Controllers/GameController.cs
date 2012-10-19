219b20c164bca2cbb898ce3918c701d6cfad1478
David Whitney <david@davidwhitney.co.uk>
2012-10-19 02:11:10 +0100

    added game creation button.

---
+++ StarBastardCore.Website/Controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace StarBastardCore.Website.Controllers
+{
+    public class GameController : Controller
+    {
+        public RedirectToRouteResult Create()
+        {
+            var uniqueGameId = Guid.NewGuid();
+            return RedirectToAction("View", new {id = uniqueGameId});
+        }
+
+        public ActionResult View(Guid id)
+        {
+            return View();
+        }
+    }
+}
